f53a86da89f98402de212c0bd1b0f6d035d30390
henrya860919 <24681357asd@gmail.com>
2026-05-14 15:37:36 +0800

    Initial Commit

---
+++ noblehall-cms-app/noblehall_cms_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  noblehall_cms_appApp.swift
+//  noblehall-cms-app
+//
+//  Created by User on 2026/5/14.
+//
+
+import SwiftUI
+
+@main
+struct noblehall_cms_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
